fb011aa05f78e68371ed793abd0f93175f1eb90a
mlind <m.lindenthal98@gmail.com>
2022-12-09 23:55:18 +0100

    day 8

--- d8/d8_p1.py
+++ d8/d8_p1.py
@@ -1,16 +1,10 @@
-import math
-import pandas as pd
 
 data = open("input.txt").read().strip().split("\n")
 grid = []
-# data = [x for x in str(data)]
-# print(data)
+
 data = [str(x) for x in data]
 
 
-# valid = 0
-# print(data)
-# #
 
 def bigger_tree_present(lst, val):
     return (any(x >= int(val) for x in lst))
@@ -19,8 +13,7 @@ def bigger_tree_present(lst, val):
 for row in range(len(data)):
     grid.append([])
     for col in range(len(data[row])):
-        # print(data[row][col])
-        # print(str(data)[row][col])
+
 
         grid[row].append(data[row][col])
 
@@ -42,10 +35,8 @@ for y in range(len(grid)):
         row.pop(x)
 
         trees_above, trees_below = col[:y], col[y:]
-        # print(trees_above, trees_below, grid[y][x])
 
         trees_left, trees_right = row[:x], row[x:]
-        # print(trees_left, trees_right, grid[y][x])
         if (
                 (bigger_tree_present(trees_above, grid[y][x]) and
                  bigger_tree_present(trees_below, grid[y][x])) and
@@ -53,34 +44,8 @@ for y in range(len(grid)):
                  bigger_tree_present(trees_right, grid[y][x]))
 
         ):
-            # print(grid[y][x])
-            #
-            # print(trees_above, trees_below)
-            # print(trees_left, trees_right)
-            continue
-            # print(row, col, grid[y][x])
-        cnt += 1
-# print([sublist[4] for index, sublist in enumerate(grid) if index % 1 == 0])
 
-# for y in range(len(data)):
-#     cnt += 1
-#
-#     if y == 0 or y == len(data)-1: continue
-#     for x in range(len(str(data[y]))):
-#         loop += 1
-#
-#         if x == 0 or x == len(str(data[y]))-1: continue
-#         if (
-#             data[x][y] < data[x][y+1] and
-#             data[x][y] < data[x][y-1] and
-#             data[x][y] < data[x+1][y] and
-#             data[x][y] < data[x-1][y]
-#
-#
-#         ):
-#             print('not vis')
-#         # print(data[y][x])
-#
-# print(cnt)
-# print(loop)
+            continue
+        cnt += 1
+
 print(cnt)
